b1f2560a663c72e97a826cb31a39a0fef1bcabe5
Haku-Nicolas-Blexmann <142597715+Haku-Nicolas-Blexmann@users.noreply.github.com>
2024-12-28 19:13:37 +0200

    Delete dist directory

--- dist/microservice/microservice.libs.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class MicroserviceLib {
-    static projectName(): Promise<string>;
-    private static getServiceURI;
-    static sendTCPMessage(service: any, messagePattern: any, payload?: any): Promise<any>;
-    private static registerService;
-    static bootstrapMicroservice(appModule: any): Promise<void>;
-}
--- dist/utils.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { INestMicroservice } from '@nestjs/common';
-export declare class MicroserviceLibModule {
-    static projectName(): Promise<string>;
-    private static registerService;
-    private static register;
-    static bootstrapMicroservice(appModule: any, customControllers?: any): Promise<INestMicroservice>;
-}
